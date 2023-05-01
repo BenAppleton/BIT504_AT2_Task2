c9824bcb4ce63e608a14fd148c27d11cf6d3735a
BenAppleton <benappleton91@gmail.com>
2023-05-01 16:56:27 +1200

    Added if statements for horizontal line and top right diagonal wins.

--- Assessment2BIT504/src/com/TicTacToe/Board.java
+++ Assessment2BIT504/src/com/TicTacToe/Board.java
@@ -40,9 +40,9 @@ public class Board {
 	    for (int row = 0; row < GameMain.ROWS; ++row) {
 	    	// This loops through each column in the row
 	        for (int col = 0; col < GameMain.COLS; ++col) {
-	        	// This checks if the content of the current cell being examined is an empty cell
+	        	// This checks if the current cell being examined is an empty cell
 	            if (cells[row][col].content == Player.Empty) {
-	            	// if an EMPTY cell is found, return false (game is not a draw)
+	            	// if an EMPTY cell is found, it will return false (game is not a draw)
 	                return false; 
 	            }
 	        }
@@ -58,9 +58,15 @@ public class Board {
 		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
 			return true; 
 		
-		 // TODO: Check if the player has 3 in the playerCol.
-		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		
+		 // Complete TODO: Check if the player has 3 in the playerCol.
+		/* This checks if the player has all 3 cells (x or o) in a column.
+		 * A column in the context of this game is a a horizontal line of cells 
+		 * While a row is a vertical line of cells
+		 */
+	    if (cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer) {
+	    	// Returning true indicates that the player won the game this way
+	        return true;
+	    }		
 		
 		
 		 // 3-in-the-diagonal
@@ -68,8 +74,11 @@ public class Board {
 			return true;
 		 
 		
-		// TODO: Check the diagonal in the other direction
-		
+		// Complete TODO: Check the diagonal in the other direction
+		// This checks for a diagonal line of 3 cells from top right to bottom left, while the above checks from top left to bottom right
+	    if (cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[2][0].content == thePlayer) {
+	        return true;
+	    }		
 
 		
 		//no winner, keep playing
